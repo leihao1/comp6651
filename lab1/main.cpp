ce05bcc23e41e4c382c270584c0cc68c0d716e6a
Lei Hao <leihao1313@gmail.com>
2018-01-18 17:42:27 -0500

    finish lab1

---
+++ lab1/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(int arg,char* argv[]){
+	cout<<"Number of arguments: "<<arg<<endl;
+	for(int i=0;i<arg;++i){
+		cout<<"Argument "<<i<<" is "<<argv[i]<<endl;
+	}
+
+	ifstream inf(argv[1]);
+	ofstream outf(argv[2]);
+	if (!inf){
+		cout<<"Input File Missing: please enter an input file\n";
+		return -1;
+	}
+	if (!outf){
+		cout<<"Output File Missing: please enter an output file\n";
+		return -1;
+	}
+
+	int N=0;//number of dataset
+	inf>>N;
+	if(N==0){//empty input file
+		outf<<"Empty Input File: please check your input file\n";
+		return -1;
+		}
+
+	for (int i=0;i<N;++i){
+		int M=0;//number of integers in the a set
+		inf>>M;
+		if(M==0){
+			outf<<"EMPTY\n";
+		}else{
+			int max;
+			inf>>max;//assume the first elements is the maximum element
+			int count=1;//how many times the maximum appeared
+			int next;//next element
+
+			for(int i=0;i<M-1;i++){
+				inf>>next;
+				if(next==max){
+					++count;
+				}
+				if(next>max){
+					max=next;
+					count=1;//reset count to 1
+				}
+			}
+			outf<<max<<" "<<count<<endl;
+		}
+	}
+	cout<<"Finished: please check your output file to see the results\n";
+	return 0;
+}
